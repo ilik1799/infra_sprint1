bae8958a115605d43bfd7a9ce18b561bec6fbf79
ilik1799 <ilik1799@gmail.com>
2023-09-06 21:07:02 +0000

    Деплой Kittygram. Вариант №2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static_backend/'
-STATIC_ROOT = BASE_DIR/'static_backend'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/kittygram/media/'
